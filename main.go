54494eed3c086a1f978fa873c93046f44678a053
Ruslan <zzzlyka221@gmail.com>
2022-09-19 21:59:30 +0500

    added add task

--- main.go
+++ main.go
@@ -4,18 +4,28 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"todoGo/data"
 )
 
 var t *template.Template
+var taskList data.Tasks
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	t.ExecuteTemplate(w, "main.html", nil)
+	t.ExecuteTemplate(w, "main.html", taskList.Tasks)
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	taskList.AddTask(r.FormValue("task"))
+	t.ExecuteTemplate(w, "main.html", taskList.Tasks)
 }
 
 func main() {
+	taskList.AddTask("hee hee")
 	t, _ = template.ParseGlob("*.html")
 
 	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/addTask", addTask)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
