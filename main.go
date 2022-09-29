c512bde0c195852b863238d30faa5a1cefaab366
Ruslan <zzzlyka221@gmail.com>
2022-09-29 23:58:13 +0500

    partly changed main to work with users (version doesn't work rn)

--- main.go
+++ main.go
@@ -10,45 +10,73 @@ import (
 )
 
 var t *template.Template
-var taskList data.Tasks
+
+// var taskList data.Tasks
+var users data.Users
+var cookieCounterTest int = 0
+
+func getUser(receivedCookies string) *data.User {
+	for _, user := range users.Users {
+		if user.Cookies == receivedCookies {
+			return &user
+		}
+	}
+
+	return nil
+}
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(taskList.Tasks)
+	cookie, err := r.Cookie("main-cookie")
+	if err != nil {
+		// cookie not found
+		cookie := &http.Cookie{
+			Name:     "main-cookie",
+			Value:    fmt.Sprintf("Cookie%d", cookieCounterTest),
+			HttpOnly: true,
+		}
+		http.SetCookie(w, cookie)
+	}
+
+	currentUser := getUser(cookie.Name)
+
 	var Lists = struct {
 		Compl   []data.Task
 		UnCompl []data.Task
 	}{
-		Compl:   taskList.GetCompletedList(),
-		UnCompl: taskList.GetUncompletedList(),
+		Compl:   currentUser.Tasks.GetCompletedList(),
+		UnCompl: currentUser.Tasks.GetUncompletedList(),
 	}
 	t.ExecuteTemplate(w, "index.html", Lists)
 }
 
 func addTask(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	taskList.AddTask(r.FormValue("task"))
+	//taskList.AddTask(r.FormValue("task"))
+	user.Tasks.AddTask(r.FormValue("task"))
+
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func deleteTask(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Println(r.Form)
 	index, _ := strconv.Atoi(r.FormValue("token"))
-	taskList.DeleteTask(index)
+	//taskList.DeleteTask(index)
+	user.Tasks.DeleteTask(index)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func markCompleted(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	index, _ := strconv.Atoi(r.FormValue("token"))
-	taskList.ChangeCompleness(index)
+	//taskList.ChangeCompleness(index)
+	user.Tasks.ChangeCompleness(index)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func main() {
-	taskList.AddTask("hee hee")
-	taskList.AddTask("hee hee2")
-	taskList.ChangeCompleness(1)
+	user.Tasks.AddTask("hee hee")
+	user.Tasks.AddTask("hee hee2")
+	user.Tasks.ChangeCompleness(1)
 	t, _ = template.ParseGlob("templates/*.html")
 
 	http.HandleFunc("/", mainPage)
